4f6f25c2050a8287c208631202343c18f67f40ae
NoName <no@email.com>
2013-12-01 16:03:32 -0300

    initial commit

---
+++ src/hexascconv/hexascconv.java
@@ -0,0 +1,12 @@
+
+package hexascconv;
+
+public class hexascconv {
+
+    public static void main(String[] args) {
+
+        maindriver md = new maindriver();
+        md.run();
+
+    }
+}
---
+++ src/hexascconv/maindriver.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hexascconv;
+
+/**
+ *
+ * @author smartcon
+ */
+public class maindriver {
+
+    private mainscreen mainscreen;
+
+    public void run(){
+
+        mainscreen = new mainscreen();
+        mainscreen.setVisible(true);
+
+
+    }
+
+}
